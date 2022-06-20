4ec4de9623fe99cb12b59eeb4fea9b7d14b8040c
Abhijit2703 <107881573+Abhijit2703@users.noreply.github.com>
2022-06-20 23:33:11 +0530

    Update GitDemo.java

--- Abhi27/src/GitDemo.java
+++ Abhi27/src/GitDemo.java
@@ -2,9 +2,19 @@
 public class GitDemo 
 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
 		
 		System.out.println("my name is Abhijit");
+		
 	}
+	public void m1()
+	{
+			System.out.println("my name is Abhijit madane");
+
+	
+	}
+	
+	
 }
